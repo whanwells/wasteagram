c58224fd886bb35573308336d7ad57cab1082a38
William Wells <whanwells@gmail.com>
2021-08-13 12:49:43 -0500

    Get data from Firestore

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,17 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import '../widgets/app_title.dart';
 import 'new_post_screen.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  static final _dateFormatter = DateFormat('EEEE, MMMM d, y');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: AppTitle(),
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData ||
+              snapshot.data?.docs == null ||
+              snapshot.data!.docs.length == 0) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final post = snapshot.data!.docs[index];
+                return ListTile(
+                    title: Text(_dateFormatter.format(post['date'].toDate())));
+              });
+        },
       ),
       floatingActionButton: Semantics(
         button: true,
