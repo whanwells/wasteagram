6fd7335ce9832b17e54d17e5be6a2be105a84da3
William Wells <whanwells@gmail.com>
2021-08-13 12:37:20 -0500

    Add const constructor to app widget

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/list_screen.dart';
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
