5f154baa9b74441491ad67ee0d6a68bf97b0f2b5
William Wells <whanwells@gmail.com>
2021-08-13 19:06:51 -0500

    Refactor image picker FAB

--- lib/widgets/image_picker_fab.dart
+++ lib/widgets/image_picker_fab.dart
@@ -9,29 +9,24 @@ class ImagePickerFab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      return Semantics(
+    return Builder(
+      builder: (context) => Semantics(
         button: true,
         onTapHint: 'Select an image',
         child: FloatingActionButton(
-          onPressed: () async {
-            final image = await _pick();
-            if (image != null) {
-              _pushNewPostScreen(context, image);
-            }
-          },
           child: Icon(Icons.photo),
+          onPressed: () async {
+            final image = await _picker.pickImage(source: ImageSource.gallery);
+            if (image == null) return;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewPostScreen(image: image),
+              ),
+            );
+          },
         ),
-      );
-    });
-  }
-
-  Future<XFile?> _pick() {
-    return _picker.pickImage(source: ImageSource.gallery);
-  }
-
-  void _pushNewPostScreen(BuildContext context, XFile image) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => NewPostScreen(image: image)));
+      ),
+    );
   }
 }
